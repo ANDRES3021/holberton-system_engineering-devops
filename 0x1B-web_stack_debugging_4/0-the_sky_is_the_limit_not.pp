0e55a36148f0c7b9d62e87d1968bd03cd5bea98f
Andres.Pardo <carlos.pardo@uptc.edu.co>
2022-05-05 18:29:06 -0500

    fix nginx traffic

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,11 @@
+#fix nginx traffic
+
+exec { 'for-nginx':
+  command => 'sed -i "s/15/8192/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+-> exec { 'restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
+}
